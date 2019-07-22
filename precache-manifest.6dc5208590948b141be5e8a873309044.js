2fd91b5ecacbc25f83a1e1fe005f25d176d30e0a
Parviz/Ibragim Bekliev <nightkon95@yandex.ru>
2019-07-22 08:34:12 +0300

    Updates

---
+++ precache-manifest.6dc5208590948b141be5e8a873309044.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224674f88b98dbe3016a0e29c8ef65b7",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "497f3e53dec57e38079f",
+    "url": "/react-todo-app/static/css/main.4a8c775b.chunk.css"
+  },
+  {
+    "revision": "6fd387079c7bda05bc8f",
+    "url": "/react-todo-app/static/js/2.63ae9df2.chunk.js"
+  },
+  {
+    "revision": "497f3e53dec57e38079f",
+    "url": "/react-todo-app/static/js/main.cc699620.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
