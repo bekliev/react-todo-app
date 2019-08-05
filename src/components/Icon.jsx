172f7e8c74b91c637da19da15498a9a01f54e44b
Parviz/Ibragim Bekliev <nightkon95@yandex.ru>
2019-08-06 01:08:00 +0300

    Fixed warnings

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -13,7 +13,7 @@ export default React.memo(function Icon({ name, size = '2rem', color = 'white' }
       </svg>
 
     case 'download':
-      return <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+      return <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
       <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4M7 10l5 5 5-5M12 15V3"/>
     </svg>
 
