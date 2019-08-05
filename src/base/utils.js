235a15634710268a545c8041df8894cb6c423183
Parviz/Ibragim Bekliev <nightkon95@yandex.ru>
2019-08-06 01:14:50 +0300

    Finished download button

--- src/base/utils.js
+++ src/base/utils.js
@@ -1,3 +1,13 @@
 import { baseSize } from './config';
 
 export const getSize = size => `${size / baseSize}rem`;
+
+export const downloadObjectAsJson = (exportObj = {}, exportName = 'data') => {
+  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj, null, 2));
+  var elAnchor = document.createElement('a');
+  elAnchor.setAttribute("href", dataStr);
+  elAnchor.setAttribute("download", exportName + ".json");
+  document.body.appendChild(elAnchor); // required for firefox
+  elAnchor.click();
+  elAnchor.remove();
+}
--- src/components/TodoApp/TodoDownload.jsx
+++ src/components/TodoApp/TodoDownload.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 import { TodoAppContext } from './state/context';
 
+import { downloadObjectAsJson } from '../../base/utils';
 import Spacer from '../Spacer';
 import Button from '../Button';
 import Icon from '../Icon';
 
 const TodoDownload = React.memo(() => {
   const { todos } = React.useContext(TodoAppContext);
+
+  const handleClick = React.useCallback(() => downloadObjectAsJson(todos), [todos]);
+
   return todos.length > 0 && (
     <>
       <Spacer size={20} />
 
-      <Button>
+      <Button onClick={handleClick}>
         <Icon name="download" /> data.json
       </Button>
     </>
