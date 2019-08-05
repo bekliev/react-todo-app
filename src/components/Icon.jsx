ca44c5b15c6515aed5cc71f17a3b918055724eab
Parviz/Ibragim Bekliev <nightkon95@yandex.ru>
2019-08-06 01:02:27 +0300

    Added download icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -12,6 +12,11 @@ export default React.memo(function Icon({ name, size = '2rem', color = 'white' }
         <path fill={color} d="M5.8 31.1c0 2.1 1.8 3.9 4 3.9h15.5c2.1 0 3.9-1.8 3.9-3.9V7.8H5.8V31zM31.1 2h-6.8l-2-1.9h-9.7l-2 2H4v3.8H31V2z" />
       </svg>
 
+    case 'download':
+      return <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4M7 10l5 5 5-5M12 15V3"/>
+    </svg>
+
     default:
       return <p>Unknown icon name: <mark>{name}</mark></p>
   };
